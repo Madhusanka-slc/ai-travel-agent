14380e2a538934a658262a823123b3bbf806418b
Chathura Madhusanka <madhusanka.slc@gmail.com>
2026-02-12 12:01:32 +0530

    28 Connect Nextjs to FastAPI

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.env import config
 
 DEBUG = config("DEBUG", cast=bool, default=False)
+FRONTEND_ORIGINS = config("FRONTEND_ORIGINS", cast=lambda x: [s.strip() for s in x.split(",")], default="http://localhost:3000")
 
 app = FastAPI()
+# Python requests (e.g., from Jupyter) don't need CORS because they run server-side, not in a browser.
+# Browser requests (e.g., from Next.js) need CORS because browsers enforce cross-origin security.
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=FRONTEND_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/hello-world")
 def read_hello_world():
--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,28 @@
-import Image from "next/image";
+"use client"; // make for frontend only
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Home() {
+  
+  const url = "http://127.0.0.1:8080/hello-world"
+  const { data, error, isLoading } = useSWR(url, fetcher)
+
+  const mainClassCSS = "flex min-h-screen flex-col items-center justify-between p-24"
+  
+  if (error) return <div className={mainClassCSS + " text-red-500"}>failed to load</div>
+  if (isLoading) return <div className={mainClassCSS}>loading...</div>
+  console.log("data: ", data)
+  const myVar = "world"
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+   
+      <main className={mainClassCSS}>
+        <h1>Hello {myVar}</h1>
+        <div>{data.Debug}</div>
+        <div>{data.hello}</div>
+        <div>{JSON.stringify(data)}</div>
+      
       </main>
-    </div>
+ 
   );
 }
